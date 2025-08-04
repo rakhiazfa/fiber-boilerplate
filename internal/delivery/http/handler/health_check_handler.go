8667b63a873bc8bb0f36df83ecd5b00c73a90e80
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2025-08-04 17:46:35 +0700

    feat: adjust health check handler

--- internal/delivery/http/handler/health_check_handler.go
+++ internal/delivery/http/handler/health_check_handler.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"time"
-
 	"github.com/gofiber/fiber/v3"
 	"github.com/rakhiazfa/fiber-boilerplate/internal/constants"
 	"github.com/rakhiazfa/fiber-boilerplate/internal/service"
@@ -20,14 +18,12 @@ func (h *HealthCheckHandler) Check(c fiber.Ctx) error {
 	err := h.healthCheckService.Check()
 	if err != nil {
 		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
-			"timestamp": time.Now(),
-			"status":    constants.HealthStatusUnhealthy,
-			"message":   err.Error(),
+			"status":  constants.HealthStatusUnhealthy,
+			"message": err.Error(),
 		})
 	}
 
 	return c.JSON(fiber.Map{
-		"timestamp": time.Now(),
-		"status":    constants.HealthStatusHealthy,
+		"status": constants.HealthStatusHealthy,
 	})
 }
