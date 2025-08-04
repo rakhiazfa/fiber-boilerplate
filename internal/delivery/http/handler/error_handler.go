acb52392d8fb0483122b95e8b276a085c85794a9
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2025-08-04 17:45:29 +0700

    feat(error-handler): add logging using logrus

--- internal/delivery/http/handler/error_handler.go
+++ internal/delivery/http/handler/error_handler.go
@@ -5,25 +5,40 @@ import (
 	"net/http"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/requestid"
+	"github.com/sirupsen/logrus"
 )
 
-type ErrorHandler struct{}
+type ErrorHandler struct {
+	log *logrus.Logger
+}
 
-func NewErrorHandler() *ErrorHandler {
-	return &ErrorHandler{}
+func NewErrorHandler(log *logrus.Logger) *ErrorHandler {
+	return &ErrorHandler{log}
 }
 
 func (h *ErrorHandler) Handle(c fiber.Ctx, err error) error {
+	statusCode := fiber.StatusInternalServerError
+
 	var fiberErr *fiber.Error
 	if errors.As(err, &fiberErr) {
-		return c.Status(fiberErr.Code).JSON(fiber.Map{
-			"error":   http.StatusText(fiberErr.Code),
-			"message": fiberErr.Message,
-		})
+		statusCode = fiberErr.Code
 	}
 
-	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-		"error":   "Internal Server Error",
+	statusMessage := http.StatusText(statusCode)
+
+	h.log.WithFields(logrus.Fields{
+		"requestId":     requestid.FromContext(c),
+		"statusCode":    statusCode,
+		"statusMessage": statusMessage,
+		"method":        c.Method(),
+		"path":          c.Path(),
+		"ip":            c.IP(),
+		"userAgent":     c.Get("User-Agent"),
+	}).Error(err.Error())
+
+	return c.Status(statusCode).JSON(fiber.Map{
+		"status":  statusMessage,
 		"message": err.Error(),
 	})
 }
