014db0b0949d1cbd5484b511c7ba77e35b7d8903
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2025-08-06 15:18:18 +0700

    feat: enable stack trace and initialize struct validator

--- internal/config/application/application.go
+++ internal/config/application/application.go
@@ -7,9 +7,11 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	recoverer "github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/gofiber/fiber/v3/middleware/requestid"
+	"github.com/rakhiazfa/fiber-boilerplate/internal/constants"
 	"github.com/rakhiazfa/fiber-boilerplate/internal/delivery/http/handler"
 	"github.com/rakhiazfa/fiber-boilerplate/internal/delivery/http/router"
 	"github.com/rakhiazfa/fiber-boilerplate/pkg/config"
+	"github.com/rakhiazfa/fiber-boilerplate/pkg/validator"
 )
 
 func New(
@@ -17,19 +19,22 @@ func New(
 	healthCheckRouter *router.HealthCheckRouter,
 ) *fiber.App {
 	app := fiber.New(fiber.Config{
-		JSONEncoder:   json.Marshal,
-		JSONDecoder:   json.Unmarshal,
-		CaseSensitive: true,
-		StrictRouting: true,
-		ErrorHandler:  errorHandler.Handle,
-		AppName:       config.Get("APP_NAME"),
+		JSONEncoder:     json.Marshal,
+		JSONDecoder:     json.Unmarshal,
+		StructValidator: validator.New(),
+		CaseSensitive:   true,
+		StrictRouting:   true,
+		ErrorHandler:    errorHandler.Handle,
+		AppName:         config.Get("APP_NAME"),
 	})
 
 	app.Use(requestid.New())
 	app.Use(logger.New(logger.Config{
 		Format: "[PID - ${pid}] [${time}] ${status} - ${method} ${path} ${latency}\n",
 	}))
-	app.Use(recoverer.New())
+	app.Use(recoverer.New(recoverer.Config{
+		EnableStackTrace: config.Get("APP_ENV") != constants.EnvironmentProduction,
+	}))
 
 	api := app.Group("/api")
 
---
+++ internal/constants/environment.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	EnvironmentProduction = "production"
+)
